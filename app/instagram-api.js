d345434b1661f4bf79796653ab00698526566c57
Maximilian Krauß <max@krauss.io>
2017-11-28 14:16:18 +0100

    Minor changes

--- app/instagram-api.js
+++
@@ -1,9 +0,0 @@
-// Theirs
-const axios = require('axios');
-
-// Mine
-const feedUrl = 'https://www.instagram.com/krauss.maximilian/media/';
-
-module.exports = async () => {
-  const instagramResult = await axios({ method: 'get', url: feedUrl });
-};
