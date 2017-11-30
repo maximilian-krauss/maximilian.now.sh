309cc8a00416ac23561591eb17a83202df961d1b
Maximilian Krauß <max@krauss.io>
2017-11-30 13:32:54 +0100

    Enclosed frontend scripts

--- assets/main.js
+++ assets/main.js
@@ -1,22 +1,24 @@
-console.log('hi');
+(function() {
+  var sendRequest = function(url) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', url, true);
+    xhr.send();
+  };
 
-var sendRequest = function(url) {
-  var xhr = new XMLHttpRequest();
-  xhr.open('POST', url, true);
-  xhr.send();
-};
+  var handleClickFromElement = function() {
+    var serviceName = this.getAttribute('data-service');
+    sendRequest('./track/' + serviceName);
+  };
 
-var handleClickFromElement = function() {
-  var serviceName = this.getAttribute('data-service');
-  sendRequest('./track/' + serviceName);
-};
+  var attachListener = function() {
+    var links = document.getElementsByClassName('service-link');
+    for (var index = 0; index < links.length; index++) {
+      var link = links[index];
+      link.addEventListener('click', handleClickFromElement);
+    }
+  };
 
-var attachListener = function() {
-  var links = document.getElementsByClassName('service-link');
-  for (var index = 0; index < links.length; index++) {
-    var link = links[index];
-    link.addEventListener('click', handleClickFromElement);
-  }
-};
+  console.log('hi');
 
-attachListener();
+  attachListener();
+})();
